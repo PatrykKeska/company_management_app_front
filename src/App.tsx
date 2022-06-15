bf401ef9d72dc0d79a0810a87fe0af5a1e243ef0
PatrykKeska <PatrykKeska4@gmail.com>
2022-06-15 23:04:53 +0200

    Component Button with basic styling applied added

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faCoffee, faBars } from '@fortawesome/free-solid-svg-icons'
+import {Button} from "./Components/Button /Button";
 function App() {
   return (
    <>
-   <h1>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores assumenda atque consectetur culpa deserunt est incidunt labore nam nostrum officia optio placeat provident sed, sint, voluptate voluptatibus voluptatum! Accusantium, id!</h1>
 
          <FontAwesomeIcon icon={faBars} />
+       <Button >Zaloguj</Button>
+       <Button small >Zaloguj</Button>
    </>
   );
 }
