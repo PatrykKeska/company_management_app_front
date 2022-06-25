ce06a018cda02e61b2420c0caa8a11afc0d2e56d
PatrykKeska <PatrykKeska4@gmail.com>
2022-06-25 17:02:15 +0200

    Form for adding new places added and connected with API.

--- src/Layouts/AddPlaceForm/AddPlaceForm.tsx
+++ src/Layouts/AddPlaceForm/AddPlaceForm.tsx
@@ -1,9 +1,13 @@
-import React from "react";
+import React, {FormEvent, useState} from "react";
 import styled from "styled-components";
 import {Input} from "../../Components/Input/Input";
 import {Button} from "../../Components/Button /Button";
 import {Img} from "../../Components/Img/Img";
 import office from '../../assets /img/office.jpeg'
+import {SinglePlaceTypes} from "../../types/Places.types";
+import {InputOnChange} from "../../types/common.types";
+import {stringify} from "querystring";
+
 
 const StyledForm = styled.form`
   padding-top: 50px;
@@ -13,17 +17,126 @@ const StyledForm = styled.form`
   flex-direction: column;
 `
 
+const StyledLabel = styled.label`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+`
+
+
 export const AddPlaceForm = () => {
 
+    const [formValues, setFormValues] = useState({
+        name: '',
+        city: '',
+        street: '',
+        buildNumber: '',
+        img: '',
+
+    } as SinglePlaceTypes);
+
+    const [loading, setLoading] = useState(false);
+
+    const handleSubmit = (e: FormEvent) => {
+        e.preventDefault()
+        setLoading(true)
+        try {
+            (async () => {
+                const response = await fetch('http://localhost:3001/add-new-place', {
+                    method: "POST",
+                    body: JSON.stringify({
+                        ...formValues
+                    }),
+                    headers: {'Content-Type': 'application/json'},
+
+                })
+
+            })()
+        } finally {
+            setLoading(false)
+            setFormValues({
+                name: '',
+                city: '',
+                street: '',
+                buildNumber: '',
+                img: '',
+
+            })
+
+        }
+    }
 
     return (
-        <StyledForm>
-            <Img src={office}/>
-            <Input placeholder={'Nazwa...'} type={'text'}/>
-            <Input placeholder={'Miasto...'} type={'text'}/>
-            <Input placeholder={'Ulica...'} type={'text'}/>
-            <Input placeholder={'Numer Budynku...'} type={'number'}/>
+        <StyledForm onSubmit={handleSubmit}>
+            {formValues.img === '' ? <Img src={office}/> : <Img  src={formValues.img}/> }
+
+
+            <StyledLabel>
+                Nazwa:
+                <Input
+                    type={'text'} name={'name'}
+                    value={formValues.name}
+                    onChange={(event: InputOnChange) => setFormValues({
+                        ...formValues,
+                        name: event.target.value
+
+                    })}/>
+            </StyledLabel>
+
+            <StyledLabel>
+                Miasto:
+                <Input
+                    type={'text'}
+                    name={'city'}
+                    value={formValues.city}
+                    onChange={(event: InputOnChange) => setFormValues({
+                        ...formValues,
+                        city: event.target.value
+
+                    })}/>
+            </StyledLabel>
+
+            <StyledLabel>
+                Ulica:
+                <Input
+                    type={'text'}
+                    name={'street'}
+                    value={formValues.street}
+                    onChange={(event: InputOnChange) => setFormValues({
+                        ...formValues,
+                        street: event.target.value
+
+                    })}/>
+            </StyledLabel>
+
+            <StyledLabel>
+                Number Budynku:
+                <Input
+                    type={'text'}
+                    name={'buildNumber'}
+                    value={formValues.buildNumber}
+                    onChange={(event: InputOnChange) => setFormValues({
+                        ...formValues,
+                        buildNumber: event.target.value,
+
+                    })}
+                />
+            </StyledLabel>
+
+            <StyledLabel>
+                Link do Zdjęcia:
+                <Input
+                    onChange={(e: InputOnChange) => setFormValues({
+                        ...formValues,
+                        img: e.target.value
+                    })}
+                    value={formValues.img}
+                    name={'img'}
+                    type={'string'}/>
+            </StyledLabel>
             <Button>Dodaj</Button>
+
         </StyledForm>
     )
 }
---
+++ src/types/common.types.ts
@@ -0,0 +1,3 @@
+import React from "react";
+
+export type InputOnChange = React.ChangeEvent<HTMLInputElement>;
