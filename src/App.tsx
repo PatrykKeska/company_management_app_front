6ac73e397ef1ea4cf11aedc3e606c07f8adb021b
PatrykKeska <PatrykKeska4@gmail.com>
2022-06-26 20:56:33 +0200

    * context created for Nav component * context created for Office component

--- src/App.tsx
+++ src/App.tsx
@@ -5,19 +5,30 @@ import {StoragePage} from "./Pages/StoragePage/StoragePage";
 import {PlacesPage} from "./Pages/PlacesPage/PlacesPage";
 import {AddItemPage} from "./Pages/AddItemPage/AddItemPage";
 import {AddPlacePage} from "./Pages/AddPlacePage/AddPlacePage";
+import {EditSinglePlace} from "./Pages/EditSinglePlace/EditSinglePlace";
+import {SinglePlaceContext} from "./context/SinglePlace/singlePlace.context";
+import {SinglePlaceTypes} from "./types/Places.types";
+import {NavContext} from "./context/nav/nav.context";
 
 
 function App() {
+    const [details, setDetails] = useState({} as SinglePlaceTypes);
+    const [isNavOpen, setIsNavOpen] = useState(true);
 
-    // @ts-ignore
+
     return (
-        <Routes>
-            <Route path='/' element={<HomePage/>}/>
-            <Route path='/storage' element={<StoragePage/>}/>
-            <Route path='/places' element={<PlacesPage/>}/>
-            <Route path='/add-items' element={<AddItemPage/>}/>
-            <Route path='/add-places' element={<AddPlacePage/>}/>
-        </Routes>
+        <NavContext.Provider value={{isNavOpen: isNavOpen, setIsNavOpen}}>
+            <SinglePlaceContext.Provider value={{details, setDetails}}>
+                <Routes>
+                    <Route path='/' element={<HomePage/>}/>
+                    <Route path='/storage' element={<StoragePage/>}/>
+                    <Route path='/places' element={<PlacesPage/>}/>
+                    <Route path='/add-items' element={<AddItemPage/>}/>
+                    <Route path='/add-places' element={<AddPlacePage/>}/>
+                    <Route path='/places/:id' element={<EditSinglePlace/>}/>
+                </Routes>
+            </SinglePlaceContext.Provider>
+        </NavContext.Provider>
     );
 }
 
