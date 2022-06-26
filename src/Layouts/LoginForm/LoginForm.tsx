425689ebb29074fc41e1c78008c9417d877085a3
PatrykKeska <PatrykKeska4@gmail.com>
2022-06-26 20:58:57 +0200

    * state for login form created * controled forme and handle added

--- src/Layouts/LoginForm/LoginForm.tsx
+++ src/Layouts/LoginForm/LoginForm.tsx
@@ -1,24 +1,49 @@
-import React from "react";
+import React, {ChangeEventHandler, useState} from "react";
 import styled from "styled-components";
 import {Input} from "../../Components/Input/Input";
 import {Button} from "../../Components/Button /Button";
+import {StyledLabel} from "../../Components/StyledLabel/StyledLabel";
+import {InputOnChange} from "../../types/common.types";
+import {set} from "react-hook-form";
 
 
 const StyledForm = styled.form`
-display: flex;
+  display: flex;
   justify-content: center;
   align-items: center;
   flex-direction: column;
 `
 
-export const LoginForm = ()=>{
+export const LoginForm = () => {
+    const [formValues, setFormValues] = useState({login: '', password: ''});
 
-
-    return(
+    return (
         <StyledForm>
-        <Input placeholder={'Login...'} type={'text'}/>
-        <Input placeholder={'Hasło...'} type={'password'}/>
-            <Button>Zaloguj</Button>
+            <StyledLabel>
+                Login:
+                <Input
+                    value={formValues.login}
+                    type={'text'} name={'login'}
+                    onChange={(event: InputOnChange) => setFormValues({
+                           ...formValues,
+                           login: event.target.value
+                       })}/>
+
+            </StyledLabel>
+
+            <StyledLabel>
+                Password:
+                <Input
+                    name={'password'}
+                    value={formValues.password}
+                    onChange={(event: InputOnChange) => setFormValues({
+                    ...formValues,
+                    password: event.target.value
+                })} type={'password'}/>
+
+            </StyledLabel>
+
+            <Button>Sign in</Button>
         </StyledForm>
     )
 }
