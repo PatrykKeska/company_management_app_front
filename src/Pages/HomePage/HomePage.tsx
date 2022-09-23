f2d2292eddbc2e6161b09f1995a358de4b26394e
PatrykKeska <PatrykKeska4@gmail.com>
2022-09-24 00:14:22 +0200

    * temporary login solution

--- src/Pages/HomePage/HomePage.tsx
+++ src/Pages/HomePage/HomePage.tsx
@@ -1,27 +1,35 @@
-import React, {useContext} from "react";
+import React from "react";
 import {Wrapper} from "../../Components/Wrapper /Wrapper";
 import {Nav} from "../../Layouts/GeneralUse/Nav/Nav";
 import {Title} from "../../Components/Title /Title";
-import {LoginForm} from "../../Layouts/GeneralUse/LoginForm/LoginForm";
-import {AuthProvider} from "../../context/AuthProvider/AuthProvider";
 import {LogoutButton} from "../../Components/logoutButton/LogoutButton";
 
 
 export const HomePage = () => {
-    const {loginStatus} = useContext(AuthProvider);
+
+    const bodyDetails = {
+        email: "testziom@gmail.com",
+        pwd: "randompassword123"
+    }
+
+    const login = async ()=> {
+        const data = await fetch('http://localhost:3001/user/login',{
+            method:"POST",
+            body: JSON.stringify(bodyDetails),
+            credentials: 'include',
+            headers: {'Content-Type': 'application/json'},
+        })
+        console.log(await data.json())
+
+    }
 
     return (
         <>
-            {loginStatus ? (
                 <Wrapper>
                     <Nav/>
-                    <Title>Inventory Management</Title>
+                    <Title onClick={login}>Inventory Management</Title>
                     <LogoutButton/>
-
-                </Wrapper>) : <Wrapper>
-                <Title>Inventory Management</Title>
-                <LoginForm/>
-            </Wrapper>}
+                </Wrapper>
 
         </>
     )
