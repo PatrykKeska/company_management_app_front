659ccd01bc6cf2d07b908a5eee67bcbfa58e7db6
PatrykKeska <PatrykKeska4@gmail.com>
2022-09-24 20:49:17 +0200

    * update product function moved for separate file

--- src/Pages/Products/functions/updateProduct.ts
+++ src/Pages/Products/functions/updateProduct.ts
@@ -1,18 +1,18 @@
-import {apiURL} from '../../../utils/api'
-import {SingleProductTypes} from '../../../types/Product.types'
+import { apiURL } from '../../../utils/api'
+import { SingleProductTypes } from '../../../types/Product.types'
 
 export const updateProduct = async (itemDetails: SingleProductTypes) => {
-    const {id, name, price, amount, dateOfBuy, file} = itemDetails
-    const formData = new FormData()
-    formData.append('name', name)
-    formData.append('id', id!)
-    formData.append('price', String(price))
-    formData.append('amount', String(amount))
-    formData.append('dateOfBuy', dateOfBuy)
-    formData.append('file', file!)
-    await fetch(`${apiURL}/products/update`, {
-        method: 'PATCH',
-        credentials: 'include',
-        body: formData,
-    })
+  const { id, name, price, amount, dateOfBuy, file } = itemDetails
+  const formData = new FormData()
+  formData.append('name', name)
+  formData.append('id', id!)
+  formData.append('price', String(price))
+  formData.append('amount', String(amount))
+  formData.append('dateOfBuy', dateOfBuy)
+  formData.append('file', file!)
+  await fetch(`${apiURL}/products/update`, {
+    method: 'PATCH',
+    credentials: 'include',
+    body: formData,
+  })
 }
