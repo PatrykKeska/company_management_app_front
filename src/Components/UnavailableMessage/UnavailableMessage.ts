392e3180f0a57bb50c383ab3a2153ece74d9af19
PatrykKeska <PatrykKeska4@gmail.com>
2022-09-27 19:58:37 +0200

    * StyledComponent added => Unavailable message

---
+++ src/Components/UnavailableMessage/UnavailableMessage.ts
@@ -0,0 +1,10 @@
+import styled from 'styled-components'
+
+export const UnavailableMessage = styled.h2`
+  position: absolute;
+  font-size: 3em;
+  color: red;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, 0%);
+`
