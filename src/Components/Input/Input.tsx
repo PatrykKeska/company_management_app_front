fec856bd7f09993e39b5c65921374c9a9dd3a526
PatrykKeska <PatrykKeska4@gmail.com>
2022-09-27 19:52:20 +0200

    * props and styles for input when is disabled

--- src/Components/Input/Input.tsx
+++ src/Components/Input/Input.tsx
@@ -6,19 +6,23 @@ interface Props {
   value?: string | number | any
   name: string
   onChange: ChangeEventHandler
+  disabled?: boolean
 }
 
-export const StyledInput = styled.input`
+export const StyledInput = styled.input<Props>`
   padding: 15px;
   min-width: 200px;
   width: 80vw;
   max-width: 500px;
   border: 0;
-  background-color: white;
+  color: ${({ disabled }) => disabled && 'white'};
+  background-color: ${({ disabled }) => (disabled ? '#000000' : 'white')};
+  filter: ${({ disabled }) => (disabled ? 'opacity(0.6)' : 'opacity(1)')};
   border-radius: 25px;
   text-align: center;
   margin: 20px 0;
   transition: 0.3s linear all;
+
   &:hover {
     box-shadow: 0 0 5px 1px black;
   }
@@ -32,6 +36,7 @@ export const Input = (props: Props) => {
       name={props.name}
       required
       type={props.type}
-    ></StyledInput>
+      disabled={props.disabled}
+    />
   )
 }
