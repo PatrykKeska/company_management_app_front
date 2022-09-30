de1edc22384e89d8c6c0c8c078d2e9d28a4d2fea
PatrykKeska <PatrykKeska4@gmail.com>
2022-09-30 21:19:38 +0200

    handle delete product function has new features :
    - are able to change a state to render Modal
    - are able to setState with message to render in Modal

--- src/Pages/FInalized/functions /DeleteProductFromPlace.ts
+++ src/Pages/FInalized/functions /DeleteProductFromPlace.ts
@@ -1,8 +1,10 @@
 import { apiURL } from '../../../utils/api'
+import { CallBackFunction } from '../../../types/CallBackFunction'
 
 export async function deleteProductFromPlace(
   placeId: string,
   productId: string,
+  setMessage: CallBackFunction,
 ) {
   const data = await fetch(`${apiURL}/product-in-places/remove`, {
     method: 'DELETE',
@@ -10,5 +12,7 @@ export async function deleteProductFromPlace(
     body: JSON.stringify({ placeId, productId }),
     headers: { 'Content-type': 'application/json' },
   })
-  return await data.json()
+  const response = await data.json()
+  setMessage({ title: response.title, message: response.message })
+  return response
 }
