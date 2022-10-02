89d10e4205b1a0dd6533378a4d44737a9be044cf
PatrykKeska <PatrykKeska4@gmail.com>
2022-10-02 20:00:40 +0200

    Added conditional rendering. if there are no places yet than user will get short message

--- src/Pages/FInalized/FinalizedPage.tsx
+++ src/Pages/FInalized/FinalizedPage.tsx
@@ -5,12 +5,12 @@ import { SingleProduct } from '../../MaterialUIComponents/productContainer'
 import { theme } from '../../MaterialUIComponents/theme/materialTheme'
 import { ThemeProvider as SCThemeProvider } from 'styled-components'
 import { StylesProvider } from '@material-ui/core/styles'
-import { Avatar, Button } from '@mui/material'
+import { Avatar, Button, Typography } from '@mui/material'
 import { Link } from 'react-router-dom'
 import { fileApi } from '../../utils/api'
 import * as React from 'react'
 import { FlexboxContainer } from '../../Components/flexboxContainer/flexboxContainer'
-import { useGetAllAssignedPlaces } from './functions /useGetAllAssignedPlaces'
+import { useGetAllAssignedPlaces } from './hooks/useGetAllAssignedPlaces'
 
 export const FinalizedPage = () => {
   useAuthCheck()
@@ -21,26 +21,33 @@ export const FinalizedPage = () => {
       <StylesProvider injectFirst>
         <SCThemeProvider theme={theme}>
           <FlexboxContainer>
-            {places.map((place) => (
-              <SingleProduct key={place.id}>
-                <Avatar
-                  sx={{ width: 70, height: 70 }}
-                  srcSet={`${fileApi}${place.img}`}
-                />
-                <p>{place.name}</p>
-                <p>City: {place.city}</p>
-                <p>Street: {place.street}</p>
-                <p>BN: {place.buildNumber}</p>
-                <Button
-                  component={Link}
-                  to={`/finalized/${place.id}`}
-                  size='small'
-                  variant='contained'
-                >
-                  Check Products
-                </Button>
-              </SingleProduct>
-            ))}
+            {places.length < 1 ? (
+              <Typography>
+                There are no ready places to see yet. Please go to the inventory
+                tab and assign items to places.
+              </Typography>
+            ) : (
+              places.map((place) => (
+                <SingleProduct key={place.id}>
+                  <Avatar
+                    sx={{ width: 70, height: 70 }}
+                    srcSet={`${fileApi}${place.img}`}
+                  />
+                  <p>{place.name}</p>
+                  <p>City: {place.city}</p>
+                  <p>Street: {place.street}</p>
+                  <p>BN: {place.buildNumber}</p>
+                  <Button
+                    component={Link}
+                    to={`/finalized/${place.id}`}
+                    size='small'
+                    variant='contained'
+                  >
+                    Check Products
+                  </Button>
+                </SingleProduct>
+              ))
+            )}
           </FlexboxContainer>
         </SCThemeProvider>
       </StylesProvider>
