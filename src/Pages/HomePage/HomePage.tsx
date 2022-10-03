b22d603a45d57c8b08e807ef1834d93dee0b4fd9
PatrykKeska <PatrykKeska4@gmail.com>
2022-10-03 16:49:59 +0200

    * HomePage modified to new API

--- src/Pages/HomePage/HomePage.tsx
+++ src/Pages/HomePage/HomePage.tsx
@@ -1,31 +1,34 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { Wrapper } from '../../Components/Wrapper /Wrapper'
 import { Nav } from '../../Layouts/GeneralUse/Nav/Nav'
 import { Title } from '../../Components/Title /Title'
+import { LoginForm } from '../../Layouts/GeneralUse/LoginForm/LoginForm'
+import { AuthProvider } from '../../context/AuthProvider/AuthProvider'
 import { LogoutButton } from '../../Components/logoutButton/LogoutButton'
+import { logoutUser } from './functions/logoutUser'
+import { useSession } from './hooks/useSession'
 
 export const HomePage = () => {
-  const bodyDetails = {
-    email: 'testziom@gmail.com',
-    pwd: 'randompassword123',
-  }
-
-  const login = async () => {
-    const data = await fetch('http://localhost:3001/user/login', {
-      method: 'POST',
-      body: JSON.stringify(bodyDetails),
-      credentials: 'include',
-      headers: { 'Content-Type': 'application/json' },
-    })
-    console.log(await data.json())
-  }
+  const { setLoginStatus } = useContext(AuthProvider)
+  const sessionStatus = useSession()
 
   return (
     <>
       <Wrapper>
-        <Nav />
-        <Title onClick={login}>Inventory Management</Title>
-        <LogoutButton />
+        {sessionStatus ? (
+          <>
+            <Nav />
+            <Title>Inventory Management</Title>
+            <form onSubmit={(e) => logoutUser(e, setLoginStatus)}>
+              <LogoutButton />
+            </form>
+          </>
+        ) : (
+          <>
+            <Title>Inventory Management</Title>
+            <LoginForm />
+          </>
+        )}
       </Wrapper>
     </>
   )
