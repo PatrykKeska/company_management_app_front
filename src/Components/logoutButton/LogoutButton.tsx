0cee26d322d7bdac725f4f20a0c10df5621d267e
PatrykKeska <PatrykKeska4@gmail.com>
2022-10-03 16:51:06 +0200

    * logout button removed unnecessary code

--- src/Components/logoutButton/LogoutButton.tsx
+++ src/Components/logoutButton/LogoutButton.tsx
@@ -1,12 +1,6 @@
-import React, { useContext } from 'react'
+import React from 'react'
 import { Button } from '../Button /Button'
-import { AuthProvider } from '../../context/AuthProvider/AuthProvider'
 
 export const LogoutButton = () => {
-  const { setLogginStatus } = useContext(AuthProvider)
-  const logOut = () => {
-    setLogginStatus(false)
-    localStorage.clear()
-  }
-  return <Button onClick={logOut}>Log out</Button>
+  return <Button type='submit'>Log out</Button>
 }
