8f30829a67e4280dc76f65ade0cf0b3371dce3a9
PatrykKeska <PatrykKeska4@gmail.com>
2022-10-03 16:49:40 +0200

    * Local Auth provided context added

--- src/App.tsx
+++ src/App.tsx
@@ -15,31 +15,39 @@ import { EditSingleItemPage } from './Pages/Products/EditSingleItemPage/EditSing
 import { InventoryPage } from './Pages/Inventory/InventoryPage'
 import { FinalizedPage } from './Pages/FInalized/FinalizedPage'
 import { FinalizedPlace } from './Pages/FInalized/FinalizedPlace'
+import { AuthProvider } from './context/AuthProvider/AuthProvider'
 
 function App() {
+  const [isLogged, setIsLogged] = useState(false)
   const [placeDetails, setPlaceDetails] = useState({} as SinglePlaceTypes)
   const [itemDetails, setItemDetails] = useState({} as SingleProductTypes)
   const [isNavOpen, setIsNavOpen] = useState(true)
 
   return (
-    <NavContext.Provider value={{ isNavOpen: isNavOpen, setIsNavOpen }}>
-      <SingleItemContext.Provider value={{ itemDetails, setItemDetails }}>
-        <SinglePlaceContext.Provider value={{ placeDetails, setPlaceDetails }}>
-          <Routes>
-            <Route path='/' element={<HomePage />} />
-            <Route path='/storage' element={<StoragePage />} />
-            <Route path='/places' element={<PlacesPage />} />
-            <Route path='/inventory' element={<InventoryPage />} />
-            <Route path='/finalized' element={<FinalizedPage />} />
-            <Route path='/finalized/:id' element={<FinalizedPlace />} />
-            <Route path='/add-items' element={<AddItemPage />} />
-            <Route path='/add-places' element={<AddPlacePage />} />
-            <Route path='/places/:id' element={<EditSinglePlacePage />} />
-            <Route path='/storage/:id' element={<EditSingleItemPage />} />
-          </Routes>
-        </SinglePlaceContext.Provider>
-      </SingleItemContext.Provider>
-    </NavContext.Provider>
+    <AuthProvider.Provider
+      value={{ loginStatus: isLogged, setLoginStatus: setIsLogged }}
+    >
+      <NavContext.Provider value={{ isNavOpen: isNavOpen, setIsNavOpen }}>
+        <SingleItemContext.Provider value={{ itemDetails, setItemDetails }}>
+          <SinglePlaceContext.Provider
+            value={{ placeDetails, setPlaceDetails }}
+          >
+            <Routes>
+              <Route path='/' element={<HomePage />} />
+              <Route path='/storage' element={<StoragePage />} />
+              <Route path='/places' element={<PlacesPage />} />
+              <Route path='/inventory' element={<InventoryPage />} />
+              <Route path='/finalized' element={<FinalizedPage />} />
+              <Route path='/finalized/:id' element={<FinalizedPlace />} />
+              <Route path='/add-items' element={<AddItemPage />} />
+              <Route path='/add-places' element={<AddPlacePage />} />
+              <Route path='/places/:id' element={<EditSinglePlacePage />} />
+              <Route path='/storage/:id' element={<EditSingleItemPage />} />
+            </Routes>
+          </SinglePlaceContext.Provider>
+        </SingleItemContext.Provider>
+      </NavContext.Provider>
+    </AuthProvider.Provider>
   )
 }
 
