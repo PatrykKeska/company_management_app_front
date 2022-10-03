606a22a6839f36d762faa85bfe3d625a589152aa
PatrykKeska <PatrykKeska4@gmail.com>
2022-10-03 09:09:12 +0200

    * All functions to handle inputs buttons etc moved to separate files

--- src/Pages/Inventory/InventoryPage.tsx
+++ src/Pages/Inventory/InventoryPage.tsx
@@ -22,90 +22,20 @@ import { FlexContainer } from '../../MaterialUIComponents/flexContainer'
 import { Container } from '../../MaterialUIComponents/container'
 import { ValueInput } from '../../MaterialUIComponents/valueInput'
 import { useAuthCheck } from '../../utils/useAuthCheck'
+import { handlePlacePick } from './functions/handlePlacePick'
+import { handleProductPick } from './functions/handleProductPick'
+import { handleSubmit } from './functions/handleSubmit'
+import { handleInput } from './functions/handleInput'
+import { UseGetAllProductPlaces } from './hooks/useGetAllProductPlaces'
 
 export const InventoryPage = () => {
-  const [products, setProducts] = useState([] as ProductToPick[])
-  const [places, setPlaces] = useState([] as PlaceToPick[])
+  useAuthCheck()
   const [message, setMessage] = useState({ title: '', message: '' })
   const [assigned, setAssigned] = useState({} as ProductInPlace)
   const [open, setOpen] = React.useState(false)
   const handleOpen = () => setOpen(true)
   const handleClose = () => setOpen(false)
-  useAuthCheck()
-  const handleProductPick = (id: string) => {
-    setAssigned({ ...assigned, amount: 0, productId: id })
-    products
-      .filter((product) => {
-        if (product.isPicked) {
-          product.isPicked = false
-        }
-        if (product.id === id) {
-          product.isPicked = true
-        }
-      })
-      .map((product) => (product.isPicked = !product.isPicked))
-  }
-
-  const handlePlacePick = (id: string) => {
-    setAssigned({ placeId: '', productId: '', amount: 0 })
-    places
-      .filter((place) => {
-        if (place.isPicked) {
-          place.isPicked = false
-        }
-        if (place.id === id) {
-          place.isPicked = true
-        }
-      })
-      .map((place) => (place.isPicked = !place.isPicked))
-    setAssigned({ ...assigned, placeId: id })
-  }
-
-  const handleSubmit = async (e: FormEvent) => {
-    e.preventDefault()
-    try {
-      const response = await fetch(`${apiURL}/product-in-places/assign`, {
-        method: 'POST',
-        credentials: 'include',
-        headers: { 'Content-type': 'application/json' },
-        body: JSON.stringify(assigned),
-      })
-      const data = await response.json()
-      if (data.isSuccess) {
-        await setMessage({ title: 'Success', message: data.message })
-      } else {
-        await setMessage({ title: 'Fail', message: data.message })
-      }
-    } catch (e) {
-      console.log(e)
-    } finally {
-      handleOpen()
-    }
-  }
-
-  const handleInput = (
-    e: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>,
-  ) => {
-    setAssigned({ ...assigned, amount: Number(e.target.value) })
-  }
-  useEffect(() => {
-    ;(async () => {
-      const fetchProducts = (await getAllProducts()) as SingleProductTypes[]
-      const fetchPlaces = (await getAllPlaces()) as SinglePlaceTypes[]
-      const singleProduct = fetchProducts
-        .filter((isAvailable) => isAvailable.productStatus === 1)
-        .map((product) => {
-          return { ...product, isPicked: false }
-        })
-      const singlePlace = fetchPlaces
-        .filter((isAvailable) => isAvailable.placeStatus === 1)
-        .map((place) => {
-          return { ...place, isPicked: false }
-        })
-      setProducts(singleProduct)
-      setPlaces(singlePlace)
-    })()
-  }, [open])
+  const { places, products } = UseGetAllProductPlaces(open)
 
   return (
     <Wrapper>
@@ -128,7 +58,14 @@ export const InventoryPage = () => {
                     <p>BN: {place.buildNumber}</p>
                     <MyButton
                       variant='contained'
-                      onClick={() => handlePlacePick(place.id!)}
+                      onClick={() =>
+                        handlePlacePick(
+                          place.id!,
+                          setAssigned,
+                          places,
+                          assigned,
+                        )
+                      }
                     >
                       Pick
                     </MyButton>
@@ -151,14 +88,23 @@ export const InventoryPage = () => {
                     <p>Date: {product.dateOfBuy}</p>
                     <MyButton
                       variant='contained'
-                      onClick={() => handleProductPick(product.id!)}
+                      onClick={() =>
+                        handleProductPick(
+                          product.id!,
+                          assigned,
+                          setAssigned,
+                          products,
+                        )
+                      }
                     >
                       Pick
                     </MyButton>
                     {product.isPicked && (
                       <>
                         <ValueInput
-                          onChange={(e) => handleInput(e)}
+                          onChange={(e) =>
+                            handleInput(e, setAssigned, assigned)
+                          }
                           fullWidth={false}
                           size='small'
                           type='number'
@@ -168,7 +114,9 @@ export const InventoryPage = () => {
                         />
                         <MyButton
                           variant='contained'
-                          onClick={handleSubmit}
+                          onClick={(e) =>
+                            handleSubmit(e, assigned, setMessage, handleOpen)
+                          }
                           size={'small'}
                         >
                           Add
