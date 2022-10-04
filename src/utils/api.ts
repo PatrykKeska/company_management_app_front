5891565f02a3ba87d3a42336386b404e90b66051
PatrykKeska <PatrykKeska4@gmail.com>
2022-10-04 14:15:22 +0200

    * updated by forcing

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,6 +1,8 @@
 // After build command this path gonna get /api
 // if your api use different path please change it.
-export const apiURL = process.env.REACT_APP_API_URL ?? 'http://localhost:3001'
-export const fileApi = 'http://localhost:3001/files/'
+// export const apiURL = process.env.REACT_APP_API_URL ?? 'http://localhost:3001'
+// export const fileApi = 'http://localhost:3001/files/'
+export const fileApi = 'http://localhost:3000/files/'
 // for localhost usage please use this version above !
-// export const apiURL = process.env.REACT_APP_API_URL ?? "http://localhost:3001/api";
+export const apiURL =
+  process.env.REACT_APP_API_URL ?? 'http://localhost:3000/api'
