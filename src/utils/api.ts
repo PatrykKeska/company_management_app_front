736689aa76d38c00e3028a0d12357293870c10aa
PatrykKeska <PatrykKeska4@gmail.com>
2022-10-05 21:18:50 +0200

    * new apis values added

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,8 +1,14 @@
 // After build command this path gonna get /api
-// if your api use different path please change it.
 // export const apiURL = process.env.REACT_APP_API_URL ?? 'http://localhost:3001'
-// export const fileApi = 'http://localhost:3001/files/'
+// if your api use different path please change it.
 export const fileApi = 'http://localhost:3001/files/'
-// for localhost usage please use this version above !
-export const apiURL =
-  process.env.REACT_APP_API_URL ?? 'http://localhost:3001/api'
+// export const fileApi = 'https://localhost:3001/files/'
+// export const fileApi = 'https://kendziior4.usermd.net/api/files/'
+// export const fetchFile = 'https://kendziior4.usermd.net/api/file-transfer'
+export const fetchFile = 'http://localhost:3001/file-transfer/photo'
+export const fetchDefaultProductImage =
+  'http://localhost:3001/file-transfer/default-product'
+export const fetchDefaultPlaceImage =
+  'http://localhost:3001/file-transfer/default-place'
+// for localhost usage please use this version below !
+export const apiURL = 'http://localhost:3001'
