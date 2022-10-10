ca9799ebe99b1b298dfbef4c9d7c55036ccfc49d
PatrykKeska <PatrykKeska4@gmail.com>
2022-10-10 19:30:25 +0200

    added interface to response

--- src/Pages/Products/functions/getAllProducts.ts
+++ src/Pages/Products/functions/getAllProducts.ts
@@ -1,9 +1,10 @@
 import { apiURL } from '../../../utils/api'
+import { SinglePlacesProductsTypes } from '../../../types/places_products.types'
 
 export const getAllProducts = async () => {
   const response = await fetch(`${apiURL}/products`, {
     credentials: 'include',
     headers: { 'Content-Type': 'application/json' },
   })
-  return await response.json()
+  return (await response.json()) as SinglePlacesProductsTypes[]
 }
