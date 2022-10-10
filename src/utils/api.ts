8a9f9c1eecc9ad9eee0a07c72cf933bd7dede283
PatrykKeska <PatrykKeska4@gmail.com>
2022-10-10 19:30:11 +0200

    cleanup in api file

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,10 +1,3 @@
-// After build command this path gonna get /api
-// export const apiURL = process.env.REACT_APP_API_URL ?? 'http://localhost:3001'
-// if your api use different path please change it.
 export const fileApi = 'https://kendziior4.usermd.net/'
-// export const fileApi = 'https://localhost:3001/files/'
-// export const fileApi = 'https://kendziior4.usermd.net/api/files/'
-// export const fetchFile = 'https://kendziior4.usermd.net/api/file-transfer'
 
-// for localhost usage please use this version below !
 export const apiURL = 'https://kendziior4.usermd.net/api'
